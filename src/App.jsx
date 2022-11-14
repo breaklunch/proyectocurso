62425a7d8ba64ab3a980845e46585e8eeeb234fb
Michardo <112974296+breaklunch@users.noreply.github.com>
2022-11-14 17:10:14 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const [criptos, setCriptos] = useState()
+
+useEffect(()=>{
+  fetch(`${API_URL}assets`)
+  .then((resp)=> resp.json())
+  .then((data)=>{
+    setCriptos(data.data)
+  })
+  .catch(()=>{
+    console.error("La peticion fallo")
+  })
+}, [])
+
+if(!criptos) return <h1>Cargando...</h1>
+
   return (
-     <h1>Hola mundo</h1>
+    <>
+     <h1>Lista de Criptomonedas</h1>
+
+      <ol>
+       { criptos.map(({name, priceUsd})=>(
+        <li>Nombre:{name} Precio:{priceUsd}</li>
+      )) }
+      </ol>
+    </>
   )
 }
 
